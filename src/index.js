a25ccddaa34db6ed40be67c6d57148a9f77c6cd6
Maksim1903 <dovgusha.m@gmail.com>
2022-12-28 22:13:07 +0100

    accomplish

--- src/index.js
+++ src/index.js
@@ -72,16 +72,15 @@ loadMoreBtn.refs.button.addEventListener('click', onClick);
 
 async function onSearch(e) {
   e.preventDefault();
-
   if (
+    photoService.query === e.currentTarget.elements.searchQuery.value.trim()
+  ) {
+    return;
+  } else if (
     photoService.query === e.currentTarget.elements.searchQuery.value.trim() &&
     !photoService.query
   ) {
     Notify.failure('Sorry, but you must enter a value');
-    return;
-  } else if (
-    photoService.query === e.currentTarget.elements.searchQuery.value.trim()
-  ) {
     return;
   }
 
